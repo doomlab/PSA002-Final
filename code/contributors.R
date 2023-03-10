97820f3ce1519d59ae1f06d086203fb67d33e569
The Doom Lab <buchananlab@gmail.com>
2023-03-10 08:48:29 -0600

    updating to almost final

---
+++ code/contributors.R
@@ -0,0 +1,22 @@
+library(googlesheets4)
+#devtools::install_github("marton-balazs-kovacs/tenzing")
+library(tenzing)
+
+DF <- read_sheet(
+  ss = "https://docs.google.com/spreadsheets/d/1EquLYV4baIjYtfF8kxs6-w-aQig32s2gRT-w4wurhhw/edit#gid=0"
+)
+
+DF$`Middle name` <- NA
+
+validate_contributors_table(contributors_table = DF)
+
+colnames(DF)[1:3] <- c("Order in publication", 
+                       "Firstname", "Surname")
+
+DF <- subset(DF, !is.na(Firstname))
+
+#DF$`Email address` <- paste0("'", DF$`Email address`, "'")
+
+cat(print_yaml(contributors_table = DF))
+
+"No. 67, Jei-Ren St., Hualien City, Taiwan"
